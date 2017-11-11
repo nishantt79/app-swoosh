eb5a8ea21eb9798b8849ec726af338f6ae33c83e
Nishant Taneja <techlabs@Techs-iMac.local>
2017-11-12 02:45:36 +0530

    minor changes

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,11 +17,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func skillVC(backsegue: UIStoryboardSegue) {
+        
     }
-
 
 }
 
